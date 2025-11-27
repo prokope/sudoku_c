6a3046ff49871687821bb13362a165b42b1d0f62
Paulo Procopio <paulorprocopio@gmail.com>
2025-11-27 11:20:54 -0300

    Saving the grids.txt file to a string array

--- sudoku.c
+++ sudoku.c
@@ -1,19 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(void)
 {
-    FILE *grids = fopen("grids.txt", "r");
+    FILE *grids = fopen("grids.txt", "r"); // Opening file with grids
 
-    if (grids == NULL)
+    if (grids == NULL) // If the file can't be read, exit
     {
         printf("Error when trying to read the file.\n");
-        return 1;
+        return 1; 
     }
 
-    char row[4];
-    while ( fgets(row, sizeof(row), grids) != NULL)
-    { 
-        // Read row
+    char row[8], grid[4][16]; // Creating a string for rows and an array of strings for the grid
+    int i = 0;
+
+    while (fgets(row, sizeof(row), grids) != NULL)
+    {
+        strcpy(grid[i], row);
+        printf("%s", grid[i]);
+        i++;
     }
+
+    fclose(grids);
     return 0;
 }
