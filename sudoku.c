ec7b1ef4b426f1e16848a3656b9b1b5020200817
Paulo Procopio <paulorprocopio@gmail.com>
2025-11-28 11:33:15 -0300

    Implementing a fix to player input and a loop to show the game grid

--- sudoku.c
+++ sudoku.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-    FILE *grids = fopen("grids.txt", "r"); // Opening file with grids
+    FILE *grids = fopen("grids.txt", "r"); // Opening file with grids and saving into a pointer grids
 
     if (grids == NULL) // If the file can't be read, exit
     {
@@ -12,59 +12,71 @@ int main(void)
     }
 
     char read_row[32], grid[4][16]; // Creating a string for rows and an array of strings for the grid
-    int i = 0;
-
+    int row = 0, column = 0;
     while (fgets(read_row, sizeof(read_row), grids) != NULL)
     {
-        strcpy(grid[i], read_row);
-        // printf("%s", grid[i]);
-        i++;
+        strcpy(grid[row], read_row);
+        row++;
     }
     fclose(grids);
 
-    int move_row, move_column, clean_row = 1, clean_column = 1;
-    char number;
+    int gameEnded = 0;
 
-    printf("Enter your number: ");
-    scanf(" %c", &number);
-
-    printf("Enter the row of your move: ");
-    scanf("%i", &move_row);
-
-    printf("Enter the column of your move: ");
-    scanf("%i", &move_column);
-
-    printf("Number: %c\nMove Row: %i\nMove column: %i\n", number, move_row, move_column);
-    
-    for (int grid_column = 0; grid_column < 7; grid_column++)
-    {   
-        if (number == grid[move_row][grid_column])
+    while (gameEnded == 0)
+    {
+        for (row = 0; row < 4; row++)
         {
-            printf("There is already a number on this row.\n");
-            clean_row = 0;
-            break;
+            for (column = 0; column < 8; column++)
+            {
+                printf("%c", grid[row][column]);
+            }
+            printf("");
+        }
+
+        int move_row, move_column, clean_row = 1, clean_column = 1;
+        char number;
+
+        printf("\nEnter your number: ");
+        scanf(" %c", &number);
+
+        printf("Enter the row of your move: ");
+        scanf("%i", &move_row);
+        move_row -= 1;
+
+        printf("Enter the column of your move: ");
+        scanf("%i", &move_column);
+        move_column = (move_column - 1) * 2;
+        
+        for (int grid_column = 0; grid_column < 7; grid_column++)
+        {   
+            if (number == grid[move_row][grid_column])
+            {
+                printf("There is already a number on this row.\n");
+                clean_row = 0;
+                break;
+            }
+        }
+
+        for (int grid_row = 0; grid_row < 4; grid_row++)
+        {
+            if (number == grid[grid_row][move_column])
+            {
+                printf("There is already a number on this column.\n");
+                clean_column = 0;
+                break;
+            }
+        }
+
+        if ( (clean_row == 1) && (clean_column == 1) )
+        {
+            printf("Success.");
+            gameEnded = 1;
+        }
+
+        else
+        {
+            printf("Mistake.\n");
         }
     }
-
-    for (int grid_row = 0; grid_row < 3; grid_row++)
-    {
-        if (number == grid[grid_row][move_column])
-        {
-            printf("There is already a number on this column.\n");
-            clean_column = 0;
-            break;
-        }
-    }
-
-    if ( (clean_row == 1) && (clean_column == 1) )
-    {
-        printf("Success.");
-    }
-
-    else
-    {
-        printf("Mistake.");
-    }
-
     return 0;
 }
